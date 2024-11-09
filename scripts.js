b45d972dc8dd297fd3537657b99b3b5217c97611
Daniel-DevCodes <carlosdanielsvieira7@gmail.com>
2024-11-08 22:45:55 -0300

    Projeto-Mario

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+const formulario = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+
+
+function cliqueiNoBotao(){
+    formulario.style.left = "50%"
+    formulario.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function cliqueiNaMascara(){
+    formulario.style.left = "-300px"
+    formulario.style.transform = "translateX(0)"
+    mascara.style.visibility = "hidden"
+
+}
